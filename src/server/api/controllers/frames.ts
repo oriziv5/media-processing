bc7ed4b7773960e0ca6fb3bdd857560a23f3f419
oriziv5 <oriziv4@gmail.com>
2022-06-21 21:44:57 +0300

    fix

--- src/server/api/controllers/frames.ts
+++ src/server/api/controllers/frames.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { createFFmpeg } from "@ffmpeg/ffmpeg";
-import moment from 'moment';
-
+import moment from "moment";
 import PQueue from "p-queue";
 let ffmpegLoadingPromise;
 let ffmpegInstance;
@@ -27,12 +26,10 @@ export async function extractFrameBySecond(req: Request, res: Response) {
   try {
     // Get the second from query
     const second = Number(req.query.s);
-    
+
     // Convert the second to ffmpeg time format
-    const time = moment().startOf('day')
-        .seconds(second)
-        .format('HH:mm:ss.SSS');
-        console.log(time);
+    const time = moment().startOf("day").seconds(second).format("HH:mm:ss.SSS");
+    console.log(time);
 
     // Read the video from buffer
     const videoData = req["file"].buffer;
@@ -46,12 +43,11 @@ export async function extractFrameBySecond(req: Request, res: Response) {
 
     // Handle several requests
     await requestQueue.add(async () => {
-        try {
-            ffmpeg.FS("writeFile", inputFileName, videoData);
-            
-        } catch (error) {
-            throw "File was not found"
-        }
+      try {
+        ffmpeg.FS("writeFile", inputFileName, videoData);
+      } catch (error) {
+        throw "File was not found";
+      }
 
       await ffmpeg.run(
         "-ss",
@@ -76,58 +72,60 @@ export async function extractFrameBySecond(req: Request, res: Response) {
     res.end(Buffer.from(outputData, "binary"));
   } catch (error) {
     console.error(error);
-    res.status(500).json({status: 500, error: error});
+    res.status(500).json({ status: 500, error: error });
   }
 }
 
 export async function extractFrameByFrameNumber(req: Request, res: Response) {
-    try {
-      // Get the second from query
-      const frame = Number(req.query.frame);
-      
-      // Read the video from buffer
-      const videoData = req["file"].buffer;
-  
-      // Create the ffmpeg instance
-      const ffmpeg = await getFFmpeg();
-  
-      const inputFileName = `input-video`;
-      const outputFileName = `thumbnail${frame}.png`;
-      let outputData = null;
-  
-      // Handle several requests
-      await requestQueue.add(async () => {
-          try {
-              ffmpeg.FS("writeFile", inputFileName, videoData);
-              
-          } catch (error) {
-              throw "File was not found"
-          }
-          //ffmpeg -i <input> -vf "select=eq(n\,34)" -vframes 1 out.png
+  const frame = Number(req.query.frame);
+  try {
+    // Get the second from query
 
-        await ffmpeg.run(
-          "-i",
-          inputFileName,
-          "-vf",
-          `select=eq(n\\,${frame})`,
-          "-frames:v",
-          "1",
-          outputFileName
-        );
-  
-        outputData = ffmpeg.FS("readFile", outputFileName);
-        ffmpeg.FS("unlink", inputFileName);
-        ffmpeg.FS("unlink", outputFileName);
-      });
-  
-      res.writeHead(200, {
-        "Content-Type": "image/png",
-        "Content-Disposition": `attachment;filename=${outputFileName}`,
-        "Content-Length": outputData.length,
-      });
-      res.end(Buffer.from(outputData, "binary"));
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({status: 500, error: error});
+    // Read the video from buffer
+    const videoData = req["file"].buffer;
+
+    // Create the ffmpeg instance
+    const ffmpeg = await getFFmpeg();
+
+    const inputFileName = `input-video`;
+    const outputFileName = `thumbnail${frame}.png`;
+    let outputData = null;
+
+    // Handle several requests
+    await requestQueue.add(async () => {
+      try {
+        ffmpeg.FS("writeFile", inputFileName, videoData);
+      } catch (error) {
+        throw "File was not found";
+      }
+      //ffmpeg -i <input> -vf "select=eq(n\,34)" -vframes 1 out.png
+
+      await ffmpeg.run(
+        "-i",
+        inputFileName,
+        "-vf",
+        `select=eq(n\\,${frame})`,
+        "-frames:v",
+        "1",
+        outputFileName
+      );
+
+      outputData = ffmpeg.FS("readFile", outputFileName);
+      ffmpeg.FS("unlink", inputFileName);
+      ffmpeg.FS("unlink", outputFileName);
+    });
+
+    res.writeHead(200, {
+      "Content-Type": "image/png",
+      "Content-Disposition": `attachment;filename=${outputFileName}`,
+      "Content-Length": outputData.length,
+    });
+    res.end(Buffer.from(outputData, "binary"));
+  } catch (error) {
+    let message: string = error.toString();
+    if (message.includes("Check if the path exists")) {
+      message = `Frame ${frame} is not exist`;
     }
+    res.status(500).json({ status: 500, error: message });
   }
+}
